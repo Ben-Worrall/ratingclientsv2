bdf5910cb67b68df73f59bb70d6af20dbe91eac4
Ben <benworrallpc@gmail.com>
2024-02-14 18:14:59 +0200

    test 2

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import {  BrowserRouter as Router,
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <React.StrictMode>
+      <React.StrictMode basename='/ratingclientsv2'>
         {<App />}
       </React.StrictMode>
       
