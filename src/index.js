feb4ecf3023f4f1186f1684d43d7c826c9d05e16
Ben <benworrallpc@gmail.com>
2024-02-14 18:25:23 +0200

    test 4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {  BrowserRouter as Router,
+import {  HashRouter as Router,
   Routes,
   Route,
   } from "react-router-dom";
@@ -13,7 +13,10 @@ import {  BrowserRouter as Router,
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
       <React.StrictMode basename='/ratingclientsv2'>
+        <Router>
         {<App />}
+        </Router>
+        
       </React.StrictMode>
       
     )
