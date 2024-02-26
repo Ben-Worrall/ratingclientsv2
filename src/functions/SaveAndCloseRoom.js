8ba46ff736d552a8bd2c20f614b9f38281601b53
Ben <benworrallpc@gmail.com>
2024-02-26 13:26:37 +0200

    successfully created copy of server with more data to SavedResults

--- src/functions/SaveAndCloseRoom.js
+++ src/functions/SaveAndCloseRoom.js
@@ -9,84 +9,131 @@ import GetResult from '../functions/GetResults';
 const db = getFirestore()
 
 
-const SaveAndCloseBNT = ()=> {
+const SaveAndCloseBNT = async ()=> {
+    
+  
+  //GET EVERYTHING THAT IS NEEDED
+
+
+  //get name of each factor
+  var FactorsListAr = JSON.parse(localStorage.getItem('factors'))
+    //sort the factors list out
+    FactorsListAr = FactorsListAr.sort((a,b) => a?.localeCompare(b, undefined, {numeric: true, sensitivity: 'base'}))
+  //get document id from server list
+  var DocumentIdServer = localStorage.getItem('DocId')
+  // get code
+  var ServerCode = localStorage.getItem('code')
+  // get username
+  var UserName = localStorage.getItem('User-Name')
+  //get password
+  var UserPassword = localStorage.getItem('User-Password')
+  //get what the user saved it as
+  var SavedAs = document.getElementById('SaveCloseName').value
+
+
+  //CREATE A NEW DOCUMENT THAT WILL BE STORED IN SAVE RESULTS
+  
+  //create a document in the "SavedResults" collection
+  const CurDocSavedResults = await addDoc(collection(db, "SavedResults"),{
+    Anchor:"Anchor"
+  })
+  
+  var DocData = {
+    Username: UserName,
+    Password: UserPassword,
+    SavedAs: SavedAs,
+    Code: ServerCode
+  }
+  for(let i = 0; i < FactorsListAr.length; i++){
+    //console.log(FactorsListAr[i])
+    let fac =  (FactorsListAr[i])
+    DocData[fac] = fac
+
+  }
+  console.log(DocData)
+  await setDoc(CurDocSavedResults, DocData)
+  
+
+  
+  //ADD SUB COLLECTIONS TO THE NEWLY CREATED DOCUMENT (in "SavedResults")
+
+  for(let i = 0; i < FactorsListAr.length; i++){
+    //for each factor, create subcollection
+    const colRef = collection(CurDocSavedResults, (FactorsListAr[i]))
+
+
+    //GET DATA FROM THE ORIGINAL SUB COLLECTION FROM THE SERVER DOC
+    
+    var CurFactorCol = collection(db,'Servers/' + DocumentIdServer + '/'+ (FactorsListAr[i]));
+    //search through the docs of the collection but pass through the host document
+    let SubDocs = await getDocs(CurFactorCol)
+    //look through the documents
+     SubDocs.forEach(async subDoc => {
+       //skip over the host doc
+       if(!subDoc.data().Host){
+        //then append each set 
+        addDoc(colRef, {
+          Notes: subDoc.data().Notes,
+          Rating: subDoc.data().Rating,
+          Username: subDoc.data().Username
+        });
+       }
+     })
+     addDoc(colRef, {
+       Anchor: "Anchor"
+    });
     
 
-
-
-    //if "Save as" input is empty, then alert and not do anything
-    //else carry on
-    if(document.getElementById('SaveCloseName').value == ""){
-        alert('Must be saved with a name')
-    }else{
-     
-        //create document in SavedResults collection
-
-const ResultsDB = collection(db, "SavedResults")
-const ResultsData = {
-    "UserName": localStorage.getItem('User-Name'),
-    "UserPassword":localStorage.getItem('User-Password')
-    
-}
-//for each factor
-var factors = JSON.parse(localStorage.getItem('factors'))
-factors = factors.sort((a,b) => a?.localeCompare(b, undefined, {numeric: true, sensitivity: 'base'}))
-factors.forEach(async (SubColId) => {
-
-
-    
-     //now that it has been established
-     //access teh sub collections to get the data
-     
-
-     // 1st step is to access the doc that matches the code
-     const colRef = collection(db, "Servers");
-     const docsSnap = await getDocs(colRef);
-     //search through and find the doc with the code
-     docsSnap.forEach(async doc => {
-        
-      //find and establish the doc of the server u made
-      if(doc.data().code == localStorage.getItem('code')){
-        var UserRatings = 0
-        var UserRatingsLength = 0
-        var UserNames = 0
-
-
-         // 2nd step is to access sub collection with id of the doc that matches the code
-         var CurFactorCol = collection(db,'Servers/' + doc.id + '/'+ SubColId);
-         //search through the docs of the collection but pass through the host document
-         let SubDocs = await getDocs(CurFactorCol)
-         //look through the documents
-          SubDocs.forEach(async subDoc => {
-            //skip over the host doc
-            if(!subDoc.data().Host){
-              UserRatings = UserRatings + Number(subDoc.data().Rating)
-              UserRatingsLength = UserRatingsLength+1
-            }
-
-          })
+  }
+  
 
 
 
 
-
-})
-
-
-
-
-console.log(ResultsData)
-addDoc(ResultsDB, ResultsData)
-
-
-
-
-//delete the document (server) from the servers collection and put code back in available codes
-
-   
-
-    }
 }
 
 
 export default SaveAndCloseBNT
+
+
+/*
+
+//GET EVERYTHING THAT IS NEEDED
+
+
+  //get name of each factor
+  var FactorsListAr = JSON.parse(localStorage.getItem('factors'))
+    //sort the factors list out
+    FactorsListAr = FactorsListAr.sort((a,b) => a?.localeCompare(b, undefined, {numeric: true, sensitivity: 'base'}))
+  //get document id from server list
+  var DocumentIdServer = localStorage.getItem('DocId')
+  // get code
+  var ServerCode = localStorage.getItem('code')
+  // get username
+  var UserName = localStorage.getItem('User-Name')
+  //get password
+  var UserPassword = localStorage.getItem('User-Password')
+  //get what the user saved it as
+  var SavedAs = document.getElementById('SavedCloseName').value
+
+
+  //CREATE A NEW DOCUMENT THAT WILL BE STORED IN SAVE RESULTS
+  
+  //create a document in the "SavedResults" collection
+  const CurDocSavedResults = await addDoc(collection(db, "SavedResults"), {
+    Username: UserName,
+    Password: UserPassword,
+    SavedAs: SavedAs,
+    Code: ServerCode
+
+  })
+  
+  var DocData = {}
+  for(let i = 0; i < FactorsListAr.length; i++){
+    DocData.FactorsListAr[i] = FactorsListAr[i]
+  }
+  
+  await setDoc(CurDocSavedResults, DocData)
+
+
+*/
