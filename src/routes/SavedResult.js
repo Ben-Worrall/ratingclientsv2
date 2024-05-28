ee4b4031c26435b8f0c515687bc9d26ea74658ea
Ben <benworrallpc@gmail.com>
2024-05-28 11:07:52 +0200

    splitting code for saved result show details

--- src/routes/SavedResult.js
+++ src/routes/SavedResult.js
@@ -11,6 +11,9 @@ import $ from 'jquery'
 import DownloadExcelFunc from '../functions/DownloadExcelSavedResult';
 import { useDownloadExcel } from 'react-export-table-to-excel';
 import EditValues from '../functions/EditValues';
+import ShowDetailsSaved from '../functions/ShowDetailsSaved'
+
+
 const db = getFirestore()
 
 
@@ -97,86 +100,7 @@ const SavedResult = () => {
  //show details function
  async function SavedShowDetails(e){
     
-    let SubColId = e.target.parentNode.firstChild.value
-    console.log(e.target.parentNode.firstChild.value)
-    document.getElementById('SavedWhite_content').style.display = ""
-    document.getElementById('SavedBlack_overlay').style.display = ""
-
-
-    //get stats from db collection
-
-
-
-    const colRef = collection(db, "SavedResults");
-    const docsSnap = await getDocs(colRef);
-    //search through and find the doc with the code
-    docsSnap.forEach(async doc => {
-        var UserName = localStorage.getItem('User-Name')
-        var UserPassword = localStorage.getItem('User-Password')
-        var SavedAs = localStorage.getItem('SavedAs-Name')
-     //find and establish the doc of the server u made
-     if(doc.data().Username == UserName){
-        if(doc.data().Password == UserPassword){
-            if(doc.data().SavedAs == SavedAs){
-
-   
-// 2nd step is to access sub collection with id of the doc that matches the code
-var CurFactorCol = collection(db,'SavedResults/' + doc.id + '/'+ SubColId);
-//search through the docs of the collection but pass through the host document
-let SubDocs = await getDocs(CurFactorCol)
-//look through the documents
- SubDocs.forEach(async subDoc => {
-            //skip over the host doc
-            if(!subDoc.data().Host){
-                if(!subDoc.data().Anchor){
-
-
-                        //div4.onclick = ShowNoteContent
-              
-             document.getElementById('SavedWhite_content').innerHTML +=(`<div id="UsernameText">${subDoc.data().Username}</div>`)
-             document.getElementById('SavedWhite_content').innerHTML +=(`<div id="RatingText">${subDoc.data().Rating}</div>`)
-         
-             document.getElementById('SavedWhite_content').innerHTML +=(
-               `<button value="${ subDoc.data().Username + "_" + SubColId}" 
-               class="SavedNotesBNTResults">
-               Notes
-               </button>`
-               )
-             
-             
-             //document.getElementById(subDoc.data().Username + "_" + SubColId).style.display = "block"
-             
-         
-         
-             //create a div for the notes for cur factor and each user
-             let CurNoteDiv = document.createElement('div')
-             CurNoteDiv.id = subDoc.data().Username + "_" + SubColId
-             CurNoteDiv.style.display = "none"
-             CurNoteDiv.classList = "SavedNotesClassListHost"
-             CurNoteDiv.innerText = subDoc.data().Notes
-             document.getElementById('root').appendChild(CurNoteDiv)
-
-
-
-                }
-              
-             }
-                         
-
-                
-
-            })
-        }
-     }
-
-     }
-
-    })
-
-
-
-
-
+  ShowDetailsSaved(e)
 
  }
 
